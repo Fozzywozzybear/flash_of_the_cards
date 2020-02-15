59d6c40cb5bfd0b7d59fd79bd21aa005b766f815
Fozzywozzybear <54587293+Fozzywozzybear@users.noreply.github.com>
2020-02-15 15:27:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flashofthecards/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.flashofthecards;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
+
+    }
+
+}
